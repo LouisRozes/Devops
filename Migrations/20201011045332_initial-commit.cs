0e4da3da4aa965605ff605d2db86fbc402a97563
Luis Rosas <20197413@itla.edu.do>
2020-12-03 21:14:23 -0400

    Devops
    Add

---
+++ Migrations/20201011045332_initial-commit.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parcial1.Migrations
+{
+    public partial class initialcommit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "vechiculos",
+                columns: table => new
+                {
+                    vehiculoid = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    placa = table.Column<string>(nullable: true),
+                    marca = table.Column<string>(nullable: true),
+                    modelo = table.Column<string>(nullable: true),
+                    color = table.Column<string>(nullable: true),
+                    descripcion = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_vechiculos", x => x.vehiculoid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "personas",
+                columns: table => new
+                {
+                    personaid = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    cedula = table.Column<string>(nullable: true),
+                    nombre = table.Column<string>(nullable: true),
+                    apellido = table.Column<string>(nullable: true),
+                    lugar = table.Column<string>(nullable: true),
+                    vehiculoid = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_personas", x => x.personaid);
+                    table.ForeignKey(
+                        name: "FK_personas_vechiculos_vehiculoid",
+                        column: x => x.vehiculoid,
+                        principalTable: "vechiculos",
+                        principalColumn: "vehiculoid",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_personas_vehiculoid",
+                table: "personas",
+                column: "vehiculoid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "personas");
+
+            migrationBuilder.DropTable(
+                name: "vechiculos");
+        }
+    }
+}
---
+++ Model.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1
+{
+    public class Db : DbContext
+    {
+        public DbSet<Vehiculo> vechiculos { get; set; }
+        public DbSet<Persona> personas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite("Data Source= Parcial1");
+    }
+
+    public class Vehiculo
+    {
+        public int vehiculoid { get; set; }
+        public string placa { get; set; }
+        public string marca { get; set; }
+        public string modelo { get; set; }
+        public string color { get; set; }
+        public string descripcion { get; set; }
+
+        public List<Persona> personas { get; } = new List<Persona>();
+
+    }
+
+    public class Persona
+    {
+        public int personaid { get; set; }
+        public string cedula  { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string lugar { get; set; }
+
+        public Vehiculo vehiculo { get; set; }
+
+    }
+
+
+}
